8b659d7d2c8b6fd18e20a568f3c88f9467ee44e6
Justin King-Lacroix <justin.kinglacroix@gmail.com>
2015-06-24 15:15:48 +0100

    netstack.h reordered for convenience

--- core/net/netstack.h
+++ core/net/netstack.h
@@ -126,8 +126,8 @@ struct network_driver {
 
 extern const struct network_driver NETSTACK_NETWORK;
 extern const struct llsec_driver   NETSTACK_LLSEC;
-extern const struct rdc_driver     NETSTACK_RDC;
 extern const struct mac_driver     NETSTACK_MAC;
+extern const struct rdc_driver     NETSTACK_RDC;
 extern const struct radio_driver   NETSTACK_RADIO;
 extern const struct framer         NETSTACK_FRAMER;
 
